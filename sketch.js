21cf7b4c58b79cca5a43cc2c05815968061a0f7f
Krish12-cpu <77144392+Krish12-cpu@users.noreply.github.com>
2021-07-15 18:22:36 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database, canvas, gameState = 0, playerCount,form,player,game
+var position;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game()
+  game.getState()
+  game.start();
+  
+}
+
+function draw(){
+}
+
+
+
